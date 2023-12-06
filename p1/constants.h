307f8b548fad4308bc82a92f0ed20ddb5b6fcf62
lucaznch <lucazanchisilva@gmail.com>
2023-12-06 10:39:44 +0000

    better main() for possible args in execution

--- p1/constants.h
+++ p1/constants.h
@@ -2,4 +2,5 @@
 #define STATE_ACCESS_DELAY_MS 10
 #define MAX_FILENAME_LENGHT 256
 #define MAX_SEAT 4 // maximum digits of a seat in the event
-#define MAX_ID 5 // maximum digits of event ID 
+#define MAX_ID 5 // maximum digits of event ID
+#define PROGRAM_EXEC_INSTRUCTIONS "4 possible combinations to execute program:\n\t./ems jobs/\n\t./ems jobs/ MAX_PROC\n\t./ems jobs/ MAX_PROC MAX_THREADS\n\t./ems jobs/ MAX_PROC MAX_THREADS [delay]\n"
--- p1/main.c
+++ p1/main.c
@@ -8,6 +8,7 @@
 #include "parser.h"
 #include "fileprocessing.h"
 
+
 int main(int argc, char *argv[]) {
   unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
 
@@ -19,28 +20,48 @@ int main(int argc, char *argv[]) {
     unsigned long int delay = strtoul(argv[1], &endptr, 10); 
 
     if (*endptr != '\0') { // fail conversion, so argv[1] is the "jobs" directory
+      
+      if (argc == 5) {
+        delay = strtoul(argv[4], &endptr, 10);
 
-      if (argc == 3) { // ./ems jobs/ MAX_PROC
+        if (delay > UINT_MAX) {
+          fprintf(stderr, "Invalid delay value or value too large\n");
+          return 1; 
+        }
+        else {
+          state_access_delay_ms = (unsigned int)delay;
+        }
+
+        // implement ex3 logic here with delay
+        printf("with delay: %u \n", state_access_delay_ms);
+        return 0;
+      }
+      
+      else if (argc == 4) {
+        // implement ex3 logic here
+        printf("no delayyyyyy !!!!\n");
+        return 0;
+      }
+
+      else if (argc == 3) {
         if (file_processing_with_processes(argv[1], state_access_delay_ms, atoi(argv[2]))) {
           fprintf(stderr, "Failed to process file\n");
           return 1;
         }
+        return 0;
       }
-      else if (argc == 2) { // ./ems jobs/
+
+      else if (argc == 2) {
         if (file_processing(argv[1], state_access_delay_ms)) {
           fprintf(stderr, "Failed to process file\n");
           return 1;
         }
+        return 0;
       }
     }
-    else if (delay > UINT_MAX) { // successful conversion but returns invalid delay
-      fprintf(stderr, "Invalid delay value or value too large\n");
-      return 1; 
-    }
-    else { // successful conversion
-      state_access_delay_ms = (unsigned int)delay;
-    }
   }
+  
+  printf(PROGRAM_EXEC_INSTRUCTIONS);
 
   return 0;
 }
