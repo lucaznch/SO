a135d027c8fab9fb4d8ba2770b5da700be0c43c6
lucaznch <lucazanchisilva@gmail.com>
2023-12-12 22:57:41 +0000

    better logic for ex3 (that doesn't work)

--- p1/fileprocessing.c
+++ p1/fileprocessing.c
@@ -278,31 +278,185 @@ int file_processing_with_processes(const char *directory_path, int max_proc, uns
 
 
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_mutex_t mutex;
+
+pthread_cond_t condition;
+
+int current_thread_id = 0;
 
 
 
-void *process_job_file_aux(void *arg) {
-    ThreadArgs *thread_args = (ThreadArgs *)arg;
 
-    pthread_mutex_lock(&mutex);
 
-    if (process_job_file((*thread_args).file_path, (*thread_args).out_fd))
-        fprintf(stderr, "Failed to process file: %s\n", (*thread_args).file_path);
-    
-    close((*thread_args).out_fd);
 
-    pthread_mutex_unlock(&mutex);
 
-    free(thread_args);
-    
-    return NULL;
+void read_to_next_line(int fd) {
+    char buf;
+    ssize_t bytesRead;
+
+    do {
+        bytesRead = read(fd, &buf, 1);
+    } while (bytesRead > 0 && buf != '\n');
 }
 
 
 
+void *process_job_file_with_thread(void *arg) {
+    ThreadArgs *thread_args = (ThreadArgs *)arg;
+    const char *file_path = (*thread_args).file_path;
+    int out_fd = (*thread_args).out_fd;
+    int thread_id = (*thread_args).thread_id;
+    int max_threads = (*thread_args).max_threads;
+    
+    int fd = open(file_path, O_RDONLY);
 
-// int process_job_file_with_threads(const char *file_path, int out_fd, int max_threads, int delayy) {}
+    int file_lines = 0;
+    int command_type;
+
+    if (fd == -1) {
+        fprintf(stderr, "Failed to open .jobs file: %s\n", file_path);
+        pthread_exit(NULL);
+    }
+
+
+    while (1) { // this loop goes line by line, using get_next(), of the ".jobs" file
+        unsigned int event_id, delay;
+        size_t num_rows, num_columns, num_coords;
+        size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
+
+        pthread_mutex_lock(&mutex);
+
+        while (current_thread_id != thread_id)
+            pthread_cond_wait(&condition, &mutex);
+
+        command_type = get_next(fd);
+        printf("(thread %d), line %d, has: %d\n",thread_id, file_lines, command_type);
+
+        if (command_type == EOC) {
+            pthread_mutex_unlock(&mutex);
+            close(fd);
+            pthread_exit(NULL);
+        }
+
+
+        if (file_lines % max_threads == thread_id) {
+            printf("thread %d will execute line %d, with: %d\n", thread_id, file_lines, command_type);
+
+            if (command_type == CMD_CREATE) {
+                if (parse_create(fd, &event_id, &num_rows, &num_columns) != 0) {
+                    fprintf(stderr, "Create: Invalid command. See HELP for usage\n");
+                    continue;
+                }
+                if (ems_create(event_id, num_rows, num_columns)) {
+                    fprintf(stderr, "Failed to create event\n");
+                }
+            } 
+            else if (command_type == CMD_RESERVE) {
+                num_coords = parse_reserve(fd, MAX_RESERVATION_SIZE, &event_id, xs, ys);
+
+                if (num_coords == 0) {
+                    fprintf(stderr, "Reserve: Invalid command. See HELP for usage\n");
+                    continue;
+                }
+                if (ems_reserve(event_id, num_coords, xs, ys)) {
+                    fprintf(stderr, "Failed to reserve seats\n");
+                }
+            } 
+            else if (command_type == CMD_SHOW) {
+                if (parse_show(fd, &event_id) != 0) {
+                    fprintf(stderr, "Show: Invalid command. See HELP for usage\n");
+                    continue;
+                }
+                if (ems_show(out_fd, event_id)) {
+                    fprintf(stderr, "Failed to show event\n");
+                }
+            } 
+            else if (command_type == CMD_LIST_EVENTS) {
+                if (ems_list_events(out_fd)) {
+                    fprintf(stderr, "Failed to list events\n");
+                }
+            } 
+            else if (command_type == CMD_WAIT) {
+                if (parse_wait(fd, &delay, NULL) == -1) {
+                    fprintf(stderr, "Wait: Invalid command. See HELP for usage\n");
+                    continue;
+                }
+                if (delay > 0) {
+                    printf("Waiting...\n");
+                    ems_wait(delay);
+                }
+            } 
+            else if (command_type == CMD_INVALID) {
+                fprintf(stderr, "Invalid: Invalid command. See HELP for usage\n");
+            } 
+            else if (command_type == CMD_HELP) {
+                printf(
+                    "Available commands:\n"
+                    "  CREATE <event_id> <num_rows> <num_columns>\n"
+                    "  RESERVE <event_id> [(<x1>,<y1>) (<x2>,<y2>) ...]\n"
+                    "  SHOW <event_id>\n"
+                    "  LIST\n"
+                    "  WAIT <delay_ms> [thread_id]\n"
+                    "  BARRIER\n"
+                    "  HELP\n");
+            } 
+            else if (command_type == CMD_BARRIER) {
+                // No action needed for these commands
+            }
+            else if (command_type == CMD_EMPTY) {
+                // idk
+            }
+            else if (command_type == EOC) {
+                pthread_mutex_unlock(&mutex);
+                close(fd);
+                pthread_exit(NULL);
+            }
+
+            current_thread_id = (current_thread_id + 1) % max_threads;
+            pthread_cond_broadcast(&condition);
+        
+        }
+        else {
+            read_to_next_line(fd);
+        }
+
+        file_lines++;
+
+        pthread_mutex_unlock(&mutex);
+        
+    } 
+}
+
+
+
+int file_processing_with_threads(const char *file_path, int out_fd, int max_threads, unsigned int delay) {
+    int i;
+    pthread_t threads[max_threads];
+    ThreadArgs thread_args;
+
+    thread_args.file_path = file_path;
+    thread_args.out_fd = out_fd;
+    thread_args.max_threads = max_threads;
+    thread_args.delay = delay;
+
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&condition, NULL);
+
+    for (i = 0; i < max_threads; i++) {
+        thread_args.thread_id = i;
+        printf("created thread %d for %s file\n", i, file_path);
+        pthread_create(&threads[i], NULL, process_job_file_with_thread, (void *)&thread_args);
+    }
+
+    for (i = 0; i < max_threads; i++)
+        pthread_join(threads[i], NULL);
+
+    pthread_cond_destroy(&condition);
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
+}
 
 
 
@@ -323,6 +477,9 @@ int file_processing_with_processes_and_threads(const char *directory_path, int m
         fprintf(stderr, "Failed to initialize EMS\n");
         return 1;
     }
+
+    //pthread_mutex_init(&mutex, NULL);
+    //pthread_cond_init(&condition, NULL);
     
     while ((entry = readdir(dir)) != NULL) { // go through all directory entries
         
@@ -355,28 +512,14 @@ int file_processing_with_processes_and_threads(const char *directory_path, int m
                 return 1;
             }
             else if (pid == 0) { // CHILD PROCESS CODE
-                int i;
-                pthread_t threads[max_threads];
-                ThreadArgs *thread_args;
-
-                for (i = 0; i < max_threads; i++) {
-                    thread_args = malloc(sizeof(ThreadArgs));
-                    (*thread_args).file_path = path;
-                    (*thread_args).out_fd = out_fd;
-
-                    if (pthread_create(&threads[i], NULL, process_job_file_aux, thread_args)) {
-                        fprintf(stderr, "Failed to create thread\n");
-                        close(out_fd);
-                        // closedir(dir);
-                        ems_terminate();
-                        exit(1);
-                    }
+                if (file_processing_with_threads(path, out_fd, max_threads, delay)) {
+                    fprintf(stderr, "Failed to process file: %s\n", path);
+                    close(out_fd);
+                    closedir(dir);
+                    ems_terminate();
+                    exit(1); 
                 }
-
-                for (i = 0; i < max_threads; i++)
-                    pthread_join(threads[i], NULL);
-                
-                closedir(dir);
+                close(out_fd);
                 ems_terminate();
                 exit(0);
             }
@@ -398,6 +541,9 @@ int file_processing_with_processes_and_threads(const char *directory_path, int m
     }
 
     ems_terminate(); // terminate the EMS state that was used by all ".jobs" files
+
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&condition);
 
     closedir(dir);
 
--- p1/fileprocessing.h
+++ p1/fileprocessing.h
@@ -3,8 +3,11 @@
 
 
 typedef struct {
-    const char *file_path; // File path of file to process
-    int out_fd; // File descriptor of corresponding ".out" file
+    const char *file_path;      // File path of file to process
+    int out_fd;                 // File descriptor of corresponding ".out" file
+    int thread_id;              // 
+    int max_threads;            //
+    unsigned int delay;         //
 } ThreadArgs;
 
 /// Creates the correspoding ".out" file for the ".jobs" file.
@@ -25,7 +28,7 @@ int process_job_file(const char *file_path, int out_fd);
 int file_processing(const char *directory_path, unsigned int delay);
 
 /// Processes all files with the ".jobs" extension in the given directory, with multiprocessing.
-/// Each file is processed in a process. Specific for exercise 2
+/// Each file is processed in one process. Specific for exercise 2
 /// @param directory_path Directory path.
 /// @param max_proc Maximum number of child processes.
 /// @param delay Delay.
@@ -38,10 +41,10 @@ int file_processing_with_processes(const char *directory_path, int max_proc, uns
 /// @param max_threads Maximum number of threads to process each file
 /// @param delay Delay.
 /// @return 0 if the all files with the ".jobs" extension were processed successfully, 1 otherwise.
-int process_job_file_with_threads(const char *file_path, int out_fd, int max_threads, int delay);
+int process_job_file_with_threads(const char *file_path, int out_fd, int max_threads, unsigned int delay);
 
 /// Processes all files with the ".jobs" extension in the given directory, with multithreaded and multiprocessing. 
-/// Each file is processed in threads in a process. Specific for exercise 3
+/// Each file is processed using multiple threads in one process. Specific for exercise 3
 /// @param directory_path Directory path.
 /// @param max_proc Maximum number of child processes.
 /// @param max_threads Maximum number of threads to process each file
